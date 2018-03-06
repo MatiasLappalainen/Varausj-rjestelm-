83e34d9e9282a2d26ce68dcc8b7e5ef8c8c331ff
Matias Lappalainen <majlap@utu.fi>
2018-03-06 23:59:41 +0200

    Aikataulut voi lukea tiedostosta

--- src/Asema.java
+++ src/Asema.java
@@ -1,14 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Asema {
 
     public String nimi;
     ArrayList<String> yhteydet;
-
+    ArrayList<String> Aikataulut;
 
     public Asema(String nimi, ArrayList yhteydet){
         this.nimi = nimi;
         this.yhteydet = yhteydet;
+        this.Aikataulut = parseAikataulut();
     }
 
 
@@ -23,6 +27,26 @@ public class Asema {
         for(String k : yhteydet){
             System.out.println(k);
         }
+    }
+
+    public ArrayList<String> annaAikataulut(){
+        return Aikataulut;
+    }
+
+    public ArrayList parseAikataulut(){
+
+
+        ArrayList<String> list = new ArrayList<String>();
+        try {
+            Scanner s = new Scanner(new File("./src/helsinki.txt"));
+            while (s.hasNext()) {
+                list.add(s.next());
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
 }
